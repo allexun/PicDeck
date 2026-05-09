2f52df59b02a5ea63d0db804bc25f2743efaa596
Aleksei Kritskov <aleksei@kritskov.dev>
2026-05-09 11:23:02 +0300

    Initial Commit

---
+++ PicDeck/PicDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicDeckApp.swift
+//  PicDeck
+//
+//  Created by Aleksei Kritskov on 09.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct PicDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
